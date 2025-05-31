8148ee5a600e8809293c0560514e8ae06378474d
L1LZ4Z <joaquinorozco2004@gmail.com>
2025-05-30 23:19:24 -0500

    chore: config project dependencies and localhost database connection

--- src/main/java/com/galaxiawonder/propgms/propgmsplatform/PropgmsPlatformApplication.java
+++ src/main/java/com/galaxiawonder/propgms/propgmsplatform/PropgmsPlatformApplication.java
@@ -1,8 +1,21 @@
+
 package com.galaxiawonder.propgms.propgmsplatform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+/**
+ * CatchUpPlatformApplication
+ *
+ * @summary
+ * The main class of the CatchUp Platform application.
+ * It is responsible for starting the Spring Boot application.
+ * It also enables JPA auditing.
+ *
+ * @since 1.0
+ */
+@EnableJpaAuditing
 @SpringBootApplication
 public class PropgmsPlatformApplication {
 
