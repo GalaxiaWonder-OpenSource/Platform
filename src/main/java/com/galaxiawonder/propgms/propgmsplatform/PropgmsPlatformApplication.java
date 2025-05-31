86417ee8dca1d600ed473f10590db92fc3ca5952
L1LZ4Z <joaquinorozco2004@gmail.com>
2025-05-30 22:27:08 -0500

    initial commit

---
+++ src/main/java/com/galaxiawonder/propgms/propgmsplatform/PropgmsPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.galaxiawonder.propgms.propgmsplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropgmsPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PropgmsPlatformApplication.class, args);
+    }
+
+}
