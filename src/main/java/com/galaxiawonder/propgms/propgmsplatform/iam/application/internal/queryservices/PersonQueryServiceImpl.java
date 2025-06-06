9b58e1e3a277557dda33664c37bb8b59129f41f5
L1LZ4Z <joaquinorozco2004@gmail.com>
2025-06-06 16:57:26 -0500

    bugfix(ep15): removed get person by name query

--- src/main/java/com/galaxiawonder/propgms/propgmsplatform/iam/application/internal/queryservices/PersonQueryServiceImpl.java
+++ src/main/java/com/galaxiawonder/propgms/propgmsplatform/iam/application/internal/queryservices/PersonQueryServiceImpl.java
@@ -3,7 +3,6 @@ package com.galaxiawonder.propgms.propgmsplatform.iam.application.internal.query
 import com.galaxiawonder.propgms.propgmsplatform.iam.domain.model.aggregates.Person;
 import com.galaxiawonder.propgms.propgmsplatform.iam.domain.model.entities.UserType;
 import com.galaxiawonder.propgms.propgmsplatform.iam.domain.model.queries.GetAllPersonsQuery;
-import com.galaxiawonder.propgms.propgmsplatform.iam.domain.model.queries.GetPersonByFirstNameQuery;
 import com.galaxiawonder.propgms.propgmsplatform.iam.domain.services.PersonQueryService;
 import com.galaxiawonder.propgms.propgmsplatform.iam.infrastructure.persitence.jpa.repositories.PersonRepository;
 import org.springframework.stereotype.Service;
@@ -32,20 +31,5 @@ public class PersonQueryServiceImpl implements PersonQueryService {
     @Override
     public List<Person> handle(GetAllPersonsQuery query) {
         return personRepository.findAll();
-    }
-
-    /**
-     * Handles the query to retrieve a user type by name.
-     *
-     * @summary
-     * Finds a user type that matches the specified name, if it exists.
-     *
-     * @param query the query object containing the user type name to search for
-     * @return an {@link Optional} containing the {@link UserType}, or empty if not found
-     * @since 1.0
-     */
-    @Override
-    public Optional<Person> handle(GetPersonByFirstNameQuery query) {
-        return personRepository.findByFirstname(query.firstName());
     }
 }
--- src/main/java/com/galaxiawonder/propgms/propgmsplatform/iam/domain/services/PersonQueryService.java
+++ src/main/java/com/galaxiawonder/propgms/propgmsplatform/iam/domain/services/PersonQueryService.java
@@ -2,10 +2,8 @@ package com.galaxiawonder.propgms.propgmsplatform.iam.domain.services;
 
 import com.galaxiawonder.propgms.propgmsplatform.iam.domain.model.aggregates.Person;
 import com.galaxiawonder.propgms.propgmsplatform.iam.domain.model.queries.GetAllPersonsQuery;
-import com.galaxiawonder.propgms.propgmsplatform.iam.domain.model.queries.GetPersonByFirstNameQuery;
 
 import java.util.List;
-import java.util.Optional;
 
 /**
  * PersonQueryService
@@ -19,15 +17,6 @@ import java.util.Optional;
  * @since 1.0
  */
 public interface PersonQueryService {
-
-    /**
-     * Retrieves a person based on their first name.
-     *
-     * @param getPersonByFirstNameQuery the query containing the first name to search for
-     * @return an Optional containing the person if found, or empty if not found
-     */
-    Optional<Person> handle(GetPersonByFirstNameQuery getPersonByFirstNameQuery);
-
     /**
      * Retrieves all persons currently stored in the system.
      *
