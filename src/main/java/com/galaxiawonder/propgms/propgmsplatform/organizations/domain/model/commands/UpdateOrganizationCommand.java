a3e6049e578873220a48d897ad30d65ffb778eb1
Henry Reaño <henry29.2004@hotmail.com>
2025-06-09 22:49:45 -0500

    feat(ep10): add update organization command service

---
+++ src/main/java/com/galaxiawonder/propgms/propgmsplatform/organizations/domain/model/commands/UpdateOrganizationCommand.java
@@ -0,0 +1,28 @@
+package com.galaxiawonder.propgms.propgmsplatform.organizations.domain.model.commands;
+
+/**
+ * Command to update an organization
+ * @param organizationId the organization id.
+ * Cannot be null or less than 1.
+ * @param commercialName the commercial name of an organization
+ * Cannot be null or blank.
+ */
+public record UpdateOrganizationCommand(Long organizationId, String commercialName) {
+    /**
+     * Constructor
+     * @param organizationId the organization id
+     *                       Cannot be null or less than 1.
+     * @param commercialName the commercial name of an organization
+     *                       Cannot be null or blank
+     * @throws IllegalArgumentException if organizationId is null or less than 1.
+     * @throws IllegalArgumentException if commercial name is null or blank
+     */
+    public UpdateOrganizationCommand {
+        if (organizationId == null || organizationId <= 0) {
+            throw new IllegalArgumentException("Organization id cannot be null or less than 1");
+        }
+        if (commercialName == null || commercialName.isBlank()) {
+            throw new IllegalArgumentException("Commercial name cannot be null or blank");
+        }
+    }
+}
