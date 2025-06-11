751d01b3844b300ff75e1f6c5f5f020a6291d276
Henry Reaño <henry29.2004@hotmail.com>
2025-06-10 19:42:05 -0500

    feat(ep11): add organization member entity and command to create an organization member

---
+++ src/main/java/com/galaxiawonder/propgms/propgmsplatform/organizations/domain/model/entities/OrganizationMember.java
@@ -0,0 +1,53 @@
+package com.galaxiawonder.propgms.propgmsplatform.organizations.domain.model.entities;
+
+import com.galaxiawonder.propgms.propgmsplatform.organizations.domain.model.aggregates.Organization;
+import com.galaxiawonder.propgms.propgmsplatform.organizations.domain.model.commands.CreateOrganizationMemberCommand;
+import com.galaxiawonder.propgms.propgmsplatform.organizations.domain.model.valueobjects.OrganizationId;
+import com.galaxiawonder.propgms.propgmsplatform.organizations.domain.model.valueobjects.OrganizationMemberType;
+import com.galaxiawonder.propgms.propgmsplatform.shared.domain.model.entities.AuditableModel;
+import com.galaxiawonder.propgms.propgmsplatform.shared.domain.model.valueobjects.PersonId;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+/**
+ * Organization Member Entity
+ *
+ * @summary
+ * The organization member class is an entity that represents an organization member.
+ * It is responsible for handling the CreateOrganizationMemberCommand command.
+ * */
+@Getter
+@Table(name = "organization_members")
+@Entity
+public class OrganizationMember extends AuditableModel {
+    @Column(nullable = false, updatable = false)
+    @Getter
+    @AttributeOverride(name = "value", column = @Column(name = "person_id"))
+    @Embedded
+    private PersonId personId;
+
+    @Column(nullable = false, updatable = false)
+    @Getter
+    @AttributeOverride(name = "value", column = @Column(name = "organization_id"))
+    @Embedded
+    private OrganizationId organizationId;
+
+    @Getter
+    @Column(nullable = false)
+    private OrganizationMemberType memberType;
+
+    protected OrganizationMember() {}
+
+    /**
+     * Constructs a new Organization Member instance by initializing its fields using the provided CreateOrganizationMemberCommand.
+     * @param command the command containing the required information to create an Organization Member.
+     *                - personId must not be null or zero
+     *                - organizationId must not be null or zero
+     *                - memberType must not be null
+     */
+    public OrganizationMember(CreateOrganizationMemberCommand command){
+        this.personId = new PersonId(command.personId());
+        this.organizationId = new OrganizationId(command.organizationId());
+        this.memberType = command.memberType();
+    }
+}
