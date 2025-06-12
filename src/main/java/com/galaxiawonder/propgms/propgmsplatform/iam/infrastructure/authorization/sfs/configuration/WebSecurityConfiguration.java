6eef0fae02cded2f3f8d67406d2a6060bc6be145
L1LZ4Z <joaquinorozco2004@gmail.com>
2025-06-11 23:28:04 -0500

    chore: add CORS configuration file for testing

--- src/main/java/com/galaxiawonder/propgms/propgmsplatform/iam/infrastructure/authorization/sfs/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/galaxiawonder/propgms/propgmsplatform/iam/infrastructure/authorization/sfs/configuration/WebSecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.galaxiawonder.propgms.propgmsplatform.iam.infrastructure.authorizati
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
@@ -20,6 +21,7 @@ import org.springframework.security.web.SecurityFilterChain;
  * @since 1.0
  */
 @Configuration
+@EnableMethodSecurity
 public class WebSecurityConfiguration {
 
     private static final String[] WHITE_LIST = {
